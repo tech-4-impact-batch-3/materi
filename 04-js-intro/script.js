24a83df5a06795b39bbcce07b030c05a31c252cd
auzanassdq <auzanassidqi@gmail.com>
2022-09-23 09:09:10 +0700

    js intro

---
+++ 04-js-intro/script.js
@@ -0,0 +1,34 @@
+// Data type
+// 1. string
+// 2. number
+// 3. boolean
+// 4. undefined
+// 5. null
+
+// Variabel
+// 1. let
+// 2. const
+// 3. var
+
+// syntax = penulisan/kaidah dari bahasa program
+
+let myName = "Alpha"
+myName = 'Beta'
+myName = `Charli`
+console.log(myName)
+
+let age = 17
+
+let isLearning = true
+let isLessThan100 = 10 < 100
+
+const NIK = "312314"
+// NIK = "9999" error
+
+// cara menampilkan
+console.log(isLessThan100);
+alert("ini error")
+let nilai = confirm("apakah anda lapar?")
+
+
+
